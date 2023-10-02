ed57286c0583984cf03e4fbc6b6e76d514740a5f
Sudhanva <sudhanvahabib@gmail.com>
2023-10-03 03:24:48 +0530

    updated

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -15,18 +15,33 @@ const OTPSchema=new mongoose.Schema({
 		expires: 60 * 5,
     }
 })
-async function sendVerificationEmail(email,otp){
-    try{
-        const mailResponse=await mailSender(email,"Verification Email",otp);
-        console.log("Email sent Successfully",mailResponse)
-    }
-    catch(error){
-        console.log("error occured while sending mails",error);
-        throw error;
-    }
+async function sendVerificationEmail(email, otp) {
+	// Create a transporter to send emails
+
+	// Define the email options
+
+	// Send the email
+	try {
+		const mailResponse = await mailSender(
+			email,
+			"Verification Email",
+			`OTP is ${otp}`
+		);
+		console.log("Email sent successfully: ", mailResponse.response);
+	} catch (error) {
+		console.log("Error occurred while sending email: ", error);
+		throw error;
+	}
 }
-OTPSchema.pre("save",async function(next){
-    await sendVerificationEmail(this.email,this.otp);
-    next();
+
+// Define a post-save hook to send email after the document has been saved
+OTPSchema.pre("save", async function (next) {
+	console.log("New document saved to database");
+
+	// Only send an email when a new document is created
+	if (this.isNew) {
+		await sendVerificationEmail(this.email, this.otp);
+	}
+	next();
 });
 module.exports=mongoose.model("OTP",OTPSchema)
